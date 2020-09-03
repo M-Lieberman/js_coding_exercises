755c06fd2e2280e71748a226b25a6bc00ad7cd6e
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-09-03 16:30:06 +0100

    implement exercise004 - completed (for loops)

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -23,33 +23,72 @@ function findNamesBeginningWith(names, char) {
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  // to something
+  let verbs = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 3 && words[i].slice(0, 3) === 'to ') {
+      verbs.push(words[i]);
+    }
+  }
+  return verbs;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let ints = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (Number.isInteger(nums[i])) {
+      ints.push(nums[i]);
+    }
+  }
+  return ints;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  let cities = [];
+  for (let i = 0; i < users.length; i++) {
+    cities.push(users[i].data.city.displayName);
+  }
+  return cities;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let sqrts = [];
+  for (let i = 0; i < nums.length; i++) {
+    let sqrt = Math.round(Math.sqrt(nums[i]) * 100) / 100;
+    sqrts.push(sqrt);
+  }
+  return sqrts;
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  let sentence = [];
+  for (let i = 0; i < sentences.length; i++) {
+    if (sentences[i].toUpperCase().indexOf(str.toUpperCase()) !== -1) {
+      sentence.push(sentences[i]);
+    }
+  }
+  return sentence;
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  let sides = [];
+  for (let i = 0; i < triangles.length; i++) {
+    let triangle = triangles[i];
+    let longest = 0;
+    for (let j = 0; j < triangle.length; j++) {
+      if (triangle[j] > longest) {
+        longest = triangle[j];
+      }
+    }
+    sides.push(longest);
+  }
+  return sides;
 }
 
 module.exports = {
