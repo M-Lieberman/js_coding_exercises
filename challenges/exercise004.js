77c7096ecdafd5221f24b7faa3f6a418c2650027
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-09-03 15:16:52 +0100

    implement exercise004 - findNamesBeginningWith

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -12,7 +12,13 @@ function findSmallNums(nums) {
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let found = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].charAt(0) == char) {
+      found.push(names[i]);
+    }
+  }
+  return found;
 }
 
 function findVerbs(words) {
